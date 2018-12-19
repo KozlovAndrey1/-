68a1ee33965708e5a1305507985ba25aa9cecc24
KozlovAndrey1 <45104419+KozlovAndrey1@users.noreply.github.com>
2018-12-19 21:18:36 +0300

    Add files via upload
    Черновик

---
+++ 1_nomer_-_s_randomom - PROBNIK.py
@@ -0,0 +1,14 @@
+import random as r
+print('Кол-во элементов:')
+N=int(input())
+L=[round(r.uniform(0,N)) for i in range(N)]
+L.sort()
+L.reverse()
+L1=L
+N1=N*0.1
+N1=round(N1)
+print(N1)
+for i in range(N-N1):
+ L1=L[0:N-1]
+ N=N-1
+print(L1)
